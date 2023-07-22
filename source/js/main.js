daacfa7d6833fe7b9f961ea8bcbf8a1b38df2113
deniskirshenin <deniskirshenin@gmail.com>
2023-07-22 13:19:21 +0400

    adding header and footer

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
+import {openClick} from './modules/menu/init-menu';
 
 // ---------------------------------
 
@@ -19,6 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+    openClick();
   });
 });
 
---
+++ source/js/modules/menu/init-menu.js
@@ -0,0 +1,50 @@
+const menuButton = document.querySelector('.nav__button');
+const logo = document.querySelector('.logo__icon');
+const navList = document.querySelector('.nav__list');
+const navLink = document.querySelectorAll('.nav__link');
+const navTel = document.querySelector('.nav__tel');
+const openIcon = document.querySelector('.nav__button-open-icon');
+const closeIcon = document.querySelector('.nav__button-close-icon');
+const nav = document.querySelector('.header');
+
+const openClick = () => {
+  menuButton.addEventListener('click', () => {
+    if (menuButton.classList.contains('nav__button--open')) {
+      nav.classList.add('header--opened');
+      navList.classList.remove('nav__list--close');
+      navList.classList.add('nav__list--open');
+      menuButton.classList.remove('nav__button--open');
+      menuButton.classList.add('nav__button--close');
+      openIcon.style = 'display: none';
+      closeIcon.style = 'display: block';
+      logo.classList.add('logo__icon--open');
+      logo.classList.remove('logo__icon--close');
+      navTel.classList.add('nav__tel--open');
+      navTel.classList.remove('nav__tel--close');
+      navLink.forEach((link) => {
+        link.classList.add('nav__link--opened');
+        link.classList.remove('btn');
+        link.classList.remove('btn--menu-link-dark');
+      });
+    } else if (menuButton.classList.contains('nav__button--close')) {
+      nav.classList.remove('header--opened');
+      navList.classList.add('nav__list--close');
+      navList.classList.remove('nav__list--open');
+      menuButton.classList.add('nav__button--open');
+      menuButton.classList.remove('nav__button--close');
+      openIcon.style = 'display: block';
+      closeIcon.style = 'display: none';
+      logo.classList.remove('logo__icon--open');
+      logo.classList.add('logo__icon--close');
+      navTel.classList.remove('nav__tel--open');
+      navTel.classList.add('nav__tel--close');
+      navLink.forEach((link) => {
+        link.classList.remove('nav__link--opened');
+        link.classList.add('btn');
+        link.classList.add('btn--menu-link-dark');
+      });
+    }
+  });
+};
+
+export {openClick};
