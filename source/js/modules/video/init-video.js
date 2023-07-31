a71dd9d77d42b11619c0b8009f90673e1aaa5038
deniskirshenin <deniskirshenin@gmail.com>
2023-07-31 23:01:45 +0400

    fixing img sizes, html code style and js for video after code review

--- source/js/modules/video/init-video.js
+++ source/js/modules/video/init-video.js
@@ -3,12 +3,14 @@ const image = document.querySelector('#video__poster');
 const button = document.querySelector('.video__play-button');
 
 const onPlayClick = () => {
-  button.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    video.style.display = 'block';
-    image.style.display = 'none';
-    button.style.display = 'none';
-  });
+  if (video && button) {
+    button.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      video.style.display = 'block';
+      image.style.display = 'none';
+      button.style.display = 'none';
+    });
+  }
 };
 
 export {onPlayClick};
